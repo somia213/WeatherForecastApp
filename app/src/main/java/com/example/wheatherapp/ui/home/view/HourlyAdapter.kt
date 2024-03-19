6c89236936f59bd2e06b8a4787e79c453015ab34
SomiaAmer <105312434+somia213@users.noreply.github.com>
2024-03-19 22:25:23 +0200

    update home fragment

--- app/src/main/java/com/example/wheatherapp/ui/home/view/HourlyAdapter.kt
+++ app/src/main/java/com/example/wheatherapp/ui/home/view/HourlyAdapter.kt
@@ -1,40 +1,42 @@
-package com.example.wheatherapp.ui.home.view
-
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.wheatherapp.data.models.ListResponse
 import com.example.wheatherapp.databinding.TimeTempCardBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
-class HourlyAdapter(var datalist:List<ListResponse>): RecyclerView.Adapter<HourlyAdapter.UserHolder>() {
+class HourlyAdapter(private val dataList: List<ListResponse>) : RecyclerView.Adapter<HourlyAdapter.UserHolder>() {
 
     class UserHolder(val binding: TimeTempCardBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(get: ListResponse) {
-            binding.textCardTime.text = get.dtTxt
-        //    binding.imageCardTempIcon
-              binding.textCardTemp.text = get.main?.temp.toString()
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("hh:mm a", Locale.getDefault())
+            val date = inputFormat.parse(get.dtTxt)
+            val formattedTime = outputFormat.format(date)
 
+            binding.textCardTime.text = formattedTime
+
+            Glide.with(binding.root.context)
+                .load("https://openweathermap.org/img/wn/${get.weather?.get(0)?.icon}@2x.png")
+                .into(binding.imageCardTempIcon)
+
+            binding.textCardTemp.text = get.main?.temp.toString()
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder {
-        val binding = TimeTempCardBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = TimeTempCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return UserHolder(binding)
     }
 
-
-
     override fun onBindViewHolder(holder: UserHolder, position: Int) {
-        holder.bind(datalist.get(position))
+        holder.bind(dataList[position])
     }
 
     override fun getItemCount(): Int {
-        return datalist.size
+        return dataList.size
     }
-
 }
-
-
-
-
--- app/src/main/java/com/example/wheatherapp/ui/home/view/SevenDayAdapter.kt
+++ app/src/main/java/com/example/wheatherapp/ui/home/view/SevenDayAdapter.kt
@@ -1,36 +1,53 @@
-package com.example.wheatherapp.ui.home.view
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.wheatherapp.data.models.ListResponse
 import com.example.wheatherapp.databinding.DayTempCardBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
+class SevenDayAdapter(private val dataList: List<ListResponse>) : RecyclerView.Adapter<SevenDayAdapter.UserHolder>() {
 
- class SevenDayAdapter(var datalist:List<ListResponse>):RecyclerView.Adapter<SevenDayAdapter.UserHolder>() {
-     class UserHolder(val binding: DayTempCardBinding) : RecyclerView.ViewHolder(binding.root) {
-         fun bind(get: ListResponse) {
-             binding.textCardDay.text = get.dtTxt
-           //  binding.imageCardDayIcon = get.weather.get(0).icon
-             binding.textCardDayTempDescription.text = get.weather?.get(0)?.description.toString()
-             binding.textCardDayTemp.text = get.main?.temp.toString()
+    class UserHolder(val binding: DayTempCardBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(get: ListResponse) {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+            val date = inputFormat.parse(get.dtTxt)
+            val formattedDay = outputFormat.format(date)
 
-         }
-     }
+            binding.textCardDay.text = formattedDay
 
-     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder {
-         val binding = DayTempCardBinding
-             .inflate(LayoutInflater.from(parent.context), parent, false)
-         return UserHolder(binding)
-     }
+            Glide.with(binding.root.context)
+                .load("https://openweathermap.org/img/wn/${get.weather?.get(0)?.icon}@2x.png")
+                .into(binding.imageCardDayIcon)
 
+            binding.textCardDayTempDescription.text = get.weather?.get(0)?.description.toString()
+            binding.textCardDayTemp.text = get.main?.temp.toString()
+        }
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserHolder {
+        val binding = DayTempCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserHolder(binding)
+    }
 
-     override fun onBindViewHolder(holder: UserHolder, position: Int) {
-         holder.bind(datalist.get(position))
-     }
+    override fun onBindViewHolder(holder: UserHolder, position: Int) {
+        val dayIndex = position % 7
+        val day = getDayOfWeek(dayIndex)
+        val dayData = dataList[dayIndex]
+        holder.bind(dayData)
+        holder.binding.textCardDay.text = day
+    }
 
-     override fun getItemCount(): Int {
-         return datalist.size
-     }
- }
+    override fun getItemCount(): Int {
+        return 7
+    }
+
+    private fun getDayOfWeek(dayIndex: Int): String {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_WEEK, dayIndex)
+        val dateFormat = SimpleDateFormat("EEEE", Locale.getDefault())
+        return dateFormat.format(calendar.time)
+    }
+}
